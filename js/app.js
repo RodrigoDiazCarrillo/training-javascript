1a1ec02c7d25005313808a36046658deb3a0c0a2
RodrigoDiazCarrillo <dzcll90@gmail.com>
2022-03-31 20:36:21 +0200

    javascript exercise

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let nav= document.querySelector("nav");
+console.log(nav);
+let active= document.querySelector(".active");
+console.log(active);
+let headersAndParagraphs = document.querySelectorAll('h1,p');
+console.log(headersAndParagraphs[1].innerText);
+console.log(active.innerText);
