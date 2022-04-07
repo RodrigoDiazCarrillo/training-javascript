8b4dcea2da6ec23051c6a58edc296ac43f57638f
RodrigoDiazCarrillo <dzcll90@gmail.com>
2022-04-07 17:13:01 +0200

    crear archivos

--- js/algorithms3.js
+++
@@ -1,9 +0,0 @@
-//1
-let min = prompt("Escriba el valor mínimo");
-let max = prompt("Escriba el valor máximo");
-let arr1 = []; 
-for (let i=0; i<20; i++){
-
-arr1[i]=Math.floor((Math.random() * (max - min + 2)) + (min+1));
-}
-console.log(arr1);
